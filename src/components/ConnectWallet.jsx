d5e8303ad535451b921a4f53ba7908885b4e5d3e
Muhammad Zain Khan <mzaink.22@gmail.com>
2026-07-06 15:21:24 +0500

    feat: wallet connect + balance display working

---
+++ src/components/ConnectWallet.jsx
@@ -0,0 +1,30 @@
+import { useAccount, useConnect, useDisconnect, useBalance } from 'wagmi'
+
+function ConnectWallet() {
+  const { address, isConnected } = useAccount()
+  const { connect, connectors } = useConnect()
+  const { disconnect } = useDisconnect()
+  const { data: balance } = useBalance({ address })
+
+  if (isConnected) {
+    return (
+      <div>
+        <p>Connected: {address}</p>
+        <p>Balance: {balance?.formatted} {balance?.symbol}</p>
+        <button onClick={() => disconnect()}>Disconnect</button>
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      {connectors.map((connector) => (
+        <button key={connector.id} onClick={() => connect({ connector })}>
+          Connect {connector.name}
+        </button>
+      ))}
+    </div>
+  )
+}
+
+export default ConnectWallet
